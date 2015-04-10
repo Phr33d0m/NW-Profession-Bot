d8b51d86bdca7358191ed0123d746c1bd08060fc
noonereally <noonereally@users.noreply.github.com>
2015-04-10 12:12:24 +0300

    small alchemy update

--- main.js
+++ main.js
@@ -1602,7 +1602,8 @@ function _select_Gateway() { // Check for Gateway used to
                 22 : ["Alchemy_Tier4_Experiment_Rank23", "Alchemy_Tier4_Experimentation_Rank22", "Alchemy_Tier4_Refine_Basic", "Alchemy_Tier4_Gather_Components"],
                 23 : ["Alchemy_Tier4_Experiment_Rank24", "Alchemy_Tier4_Experimentation_Rank23", "Alchemy_Tier4_Refine_Basic", "Alchemy_Tier4_Gather_Components"],
                 24 : ["Alchemy_Tier4_Experiment_Rank25", "Alchemy_Tier4_Experimentation_Rank24", "Alchemy_Tier4_Refine_Basic", "Alchemy_Tier4_Gather_Components"],
-			},
+                25 : ["Alchemy_Tier4_Experimentation_Rank25", "Alchemy_Tier4_Create_Elemental_Unified", "Alchemy_Tier4_Create_Elemental_Aggregate", "Alchemy_Tier4_Refine_Basic", "Alchemy_Tier4_Gather_Components"], 			
+            },
 		}]
 	};
 	addProfileToDefined("Alchemy", {
