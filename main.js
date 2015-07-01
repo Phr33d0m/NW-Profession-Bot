2c021cd3687e6af77ee3bbda81d2fd0d40be5012
EffnL <EffnL@users.noreply.github.com>
2015-07-01 02:14:33 -0700

    Slot Tracker Update
    Enabled differentiation between unassigned (open) and unassigned (locked) slots. This should aid users in recognizing available slots which are not in use.

--- main.js
+++ main.js
@@ -3126,7 +3126,13 @@ function _select_Gateway() { // Check for Gateway used to
 
         // Slot assignment
         unsafeWindow.client.dataModel.model.ent.main.itemassignments.assignments.forEach(function(slot, ix) {
-            charStatisticsList[curCharName].slotUse[ix] = slot.category;
+            if (!slot.islockedslot && slot.category !== "None") {
+                charStatisticsList[curCharName].slotUse[ix] = slot.category;
+            } else if (slot.islockedslot) {
+                charStatisticsList[curCharName].slotUse[ix] = "----"; // Locked Slot
+            } else {
+                charStatisticsList[curCharName].slotUse[ix] = "OPEN"; // Un-Assigned Slot!!!
+            }
         });
 
         // Workers and tools assignment and qty
