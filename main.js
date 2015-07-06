6474d30b81dad972a935d6b030b0b7cdb61949b4
Harley-Nation <justin@nyster.biz>
2015-07-06 13:58:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5152,9 +5152,21 @@ function addProfile(profession, profile, base){
                 limit: 0
             };
             _vendorItems[_vendorItems.length] = {
-                pattern: /_Green_T[1-5]_Unid$/,
+                pattern: /_Green_T[1-7]_Unid$/,
                 limit: 0
             }; // Unidentified Green Gear
+            _vendorItems[_vendorItems.length] = {
+                pattern: /^Object_Trinket_/,
+                limit: 0
+            }; 
+             _vendorItems[_vendorItems.length] = {
+                pattern: /^Object_Skill_/,
+                limit: 0
+            };
+             _vendorItems[_vendorItems.length] = {
+                pattern: /^Magic Focus$/,
+                limit: 0
+            };
         }
         
         if (getSetting('vendorSettings', 'vendorProfResults')) {
