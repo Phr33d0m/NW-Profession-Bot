0346a7b1023c31e12dfaa1e35e80a546e46023ad
BigRedBot <bigredbot@users.noreply.github.com>
2016-07-09 16:07:56 -0400

    edit to version number

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
 // @originalAuthor Mustex/Bunta
 // @modifiedBy NW gateway Professions Bot Developers & Contributors
 
-// @version 4.9.9
+// @version 4.9.10
 // @license http://creativecommons.org/licenses/by-nc-sa/3.0/us/
 // @grant GM_getValue
 // @grant GM_setValue
